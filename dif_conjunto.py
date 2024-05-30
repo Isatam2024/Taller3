782c353b838813f33dae65c9a7dc216e84946b5d
Isatam2024 <160289429+Isatam2024@users.noreply.github.com>
2024-05-29 22:05:49 -0500

    dif_conjunto

---
+++ dif_conjunto.py
@@ -0,0 +1,13 @@
+#5.	Encontrar el conjunto de elementos que están en un conjunto dado, pero no en otro conjunto dado.
+#conjunto_1 = {1, 2, 3, 4, 5}
+#conjunto_2 = {2, 4, 6, 8, 10}
+
+# Definimos dos conjuntos
+conjunto_A = {1, 2, 3, 4, 5}
+conjunto_B = {2, 4, 6, 8, 10}
+
+# Encontramos la diferencia entre los dos conjuntos
+diferencia = conjunto_A - conjunto_B
+
+print("Elementos que están en A pero no en B:", diferencia)
+
