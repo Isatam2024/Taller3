cb7e1c593bd6de96a2a5af9a34763d436eb31105
Isatam2024 <160289429+Isatam2024@users.noreply.github.com>
2024-05-29 22:08:05 -0500

    conjuntos_elementos

---
+++ conjunto_elementos.py
@@ -0,0 +1,12 @@
+#Encontrar el conjunto de elementos que están en al menos uno de los conjuntos dados
+#conjunto_1 = {1, 2, 3, 4, 5}
+#conjunto_2 = {2, 4, 6, 8, 10}
+
+# Definimos dos conjuntos
+conjunto_1 = {1, 2, 3, 4, 5}
+conjunto_2 = {2, 4, 6, 8, 10}
+
+# Encontramos la unión de los dos conjuntos
+union = conjunto_1.union(conjunto_2)
+
+print("Elementos que están en al menos uno de los conjuntos:", union)
