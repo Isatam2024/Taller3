2f52138f24f35b4e7fe51823d1279b7d6ce37991
Isatam2024 <160289429+Isatam2024@users.noreply.github.com>
2024-05-29 22:09:53 -0500

    max_min

---
+++ max_min.py
@@ -0,0 +1,11 @@
+#7.	Encontrar el máximo y el mínimo de una tupla de números
+
+# Definimos una tupla de números
+numeros = (3, 1, 4, 10, 2, 7, 9)
+
+# Encontramos el máximo y el mínimo
+maximo = max(numeros)
+minimo = min(numeros)
+
+print("Máximo:", maximo)
+print("Mínimo:", minimo)
